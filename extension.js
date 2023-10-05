2cbf3e26414e32474fdbac8615a90b0c7b014e1b
tian-xo <spacetian.channel@gmail.com>
2023-10-05 13:44:46 +0700

    debug keyboard selection

--- extension.js
+++ extension.js
@@ -1,36 +1,28 @@
 const vscode = require('vscode');
-const path = require('./src/path.js')
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-
-const {
-	window,
-	commands
-} = vscode
+const path = require('./src/path.js');
 
 function activate(context) {
-
-	// const output = window.createOutputChannel('debug')
-	const editor = window.activeTextEditor;
-
-	let disposable = commands.registerCommand('selection-minify', function () {
+	let disposable = vscode.commands.registerCommand('selection-minify', function () {
+		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const selection = editor.selection
-			const getSelection = editor.document.getText(selection)
-			const minified = path.minify(getSelection)
-			// output.appendLine(minified)
-			// output.show()
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, minified);
-			}).then(success => {
-				if (success) {
-					 window.showInformationMessage('Selection Minified!');
-				} else {
-					 window.showErrorMessage('Failed to minify selection.');
-				}
-		  })
+			const selection = editor.selection;
+			const selectedText = editor.document.getText(selection);
+			
+			// Delay the processing by 50 milliseconds
+			setTimeout(() => {
+				const minified = path.minify(selectedText);
+				editor.edit(editBuilder => {
+					editBuilder.replace(selection, minified);
+				}).then(success => {
+					if (success) {
+						vscode.window.showInformationMessage('Selection Minified!');
+					} else {
+						vscode.window.showErrorMessage('Failed to minify selection.');
+					}
+				});
+			}, 50);
+		} else {
+			vscode.window.showWarningMessage('No active text editor found.');
 		}
 	});
 
@@ -39,4 +31,4 @@ function activate(context) {
 
 module.exports = {
 	activate
-}
+};
